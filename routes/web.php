01310930708170ac47d291797eae4e5e4d472d51
SMel1307 <melimin2010@gmail.com>
2024-03-18 23:38:46 -0400

    agregacion iteracion 2

--- routes/web.php
+++ routes/web.php
@@ -37,15 +37,6 @@ Route::get('orders/create', function (){
  
 })->name('orders.create');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
 
 Route::get('prueba', function(){
     Cart::instance('shopping');
